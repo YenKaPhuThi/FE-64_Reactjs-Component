88e53516613deef4c2909f2fca2c15e405d0482c
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-06-13 09:38:49 +0700

    [Reactjs]: Adjust carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -11,33 +11,30 @@ import SlideThird from "../assets/images/slide_3.jpg";
 export default class Carousel extends Component {
   render() {
     return (
-      <div id="demo" className="carousel slide" data-ride="carousel">
-        {/* Indicators */}
+      <section id="carousel" className="carousel slide" data-ride="carousel">
         <ul className="carousel-indicators">
-          <li data-target="#demo" data-slide-to={0} className="active" />
-          <li data-target="#demo" data-slide-to={1} />
-          <li data-target="#demo" data-slide-to={2} />
+          <li data-target="#carousel" data-slide-to={0} className="active" />
+          <li data-target="#carousel" data-slide-to={1} />
+          <li data-target="#carousel" data-slide-to={2} />
         </ul>
-        {/* The slideshow */}
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={SlideFirst} alt="Los Angeles" width={1100} height={500} />
+            <img src={SlideFirst} alt="Los Angeles" />
           </div>
           <div className="carousel-item">
-            <img src={SlideSecond} alt="Chicago" width={1100} height={500} />
+            <img src={SlideSecond} alt="Chicago" />
           </div>
           <div className="carousel-item">
-            <img src={SlideThird} alt="New York" width={1100} height={500} />
+            <img src={SlideThird} alt="New York" />
           </div>
         </div>
-        {/* Left and right controls */}
-        <a className="carousel-control-prev" href="#demo" data-slide="prev">
+        <a className="carousel-control-prev" href="#carousel" data-slide="prev">
           <span className="carousel-control-prev-icon" />
         </a>
-        <a className="carousel-control-next" href="#demo" data-slide="next">
+        <a className="carousel-control-next" href="#carousel" data-slide="next">
           <span className="carousel-control-next-icon" />
         </a>
-      </div>
+      </section>
     );
   }
 }
